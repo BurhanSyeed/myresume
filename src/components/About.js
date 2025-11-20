ac78ef2164c29c1dc2f7a343d6dadcacf2bc90b1
BanniSyed <157731908+BurhanSyeed@users.noreply.github.com>
2025-11-20 15:47:56 +0500

    myresume

--- src/components/About.js
+++ src/components/About.js
@@ -34,16 +34,9 @@ function About() {
               <hr style={{ width: "20%" }} />
             </span>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Laravel developer with 2+ years of full-stack experience, building
+              complex systems, robust APIs, multi-role admin panels, and
+              Education & Hotel Management platforms.
             </p>
             <label htmlFor="customRange1" class="form-label">
               HTML
@@ -134,7 +127,7 @@ function About() {
               >
                 50%
               </div>
-            </div>     
+            </div>
             <a
               className="btn btn-sm"
               style={{
